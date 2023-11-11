e2efcfd57770c133c57d4584d1400c63edb8fd1a
Alexey Zholtikov <git@zh.com.ru>
2023-11-11 14:41:23 +0300

    Update 'install.sh'

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-git clone -b esp32 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow_led.git
+git clone -b esp8266 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow_led.git
 cd zh_espnow_led
 mkdir components
 cd components
 git clone http://git.zh.com.ru/alexey.zholtikov/zh_config.git
-git clone -b esp32 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow.git
+git clone -b esp8266 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow.git
 cd ..
