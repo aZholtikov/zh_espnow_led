5c6c3e2cfc9a1e0fce229770d80bd0024e8ade5e
Alexey Zholtikov <git@zh.com.ru>
2024-01-08 10:48:20 +0300

    Version 1.4.0
    Removed zh_espnow.
    Added zh_network.
    Minor main code refactoring.

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ cd zh_espnow_led
 mkdir components
 cd components
 git clone http://git.zh.com.ru/alexey.zholtikov/zh_config.git
-git clone -b esp8266 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow.git
+git clone http://git.zh.com.ru/alexey.zholtikov/zh_vector.git
+git clone -b esp8266 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_network.git
 cd ..
